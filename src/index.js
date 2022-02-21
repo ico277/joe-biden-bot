b3a95970d716499400fb84e841bc402516a7ecd5
ico277 <enfrico123@gmail.com>
2022-02-21 19:47:55 +0100

    v1.1.5

--- src/index.js
+++ src/index.js
@@ -193,7 +193,7 @@ client.on('interactionCreate', async interaction => {
                 let posts = json["posts"];
                 if (posts) {
                     let post = posts[Math.floor((Math.random() * posts.length))];
-                    if (post)
+                    if (post.file)
                         await interaction.reply(post.file.url);
                     else
                         await interaction.reply('Could not find any posts with those tags! be sure that tag exists!');
@@ -223,7 +223,7 @@ client.on('interactionCreate', async interaction => {
                 let posts = json["posts"];
                 if (posts) {
                     let post = posts[Math.floor((Math.random() * posts.length))];
-                    if (post)
+                    if (post.file)
                         await interaction.reply(post.file.url);
                     else
                         await interaction.reply('Could not find any posts with those tags! be sure that tag exists!');
